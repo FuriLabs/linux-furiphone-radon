da4f74155bb0bdd63b9fa5bf9a431f4d46d0ecb9
Aleksa Sarai <cyphar@cyphar.com>
2019-12-07 01:13:30 +1100

    namei: LOOKUP_NO_SYMLINKS: block symlink resolution
    /* Background. */
    Userspace cannot easily resolve a path without resolving symlinks, and
    would have to manually resolve each path component with O_PATH and
    O_NOFOLLOW. This is clearly inefficient, and can be fairly easy to screw
    up (resulting in possible security bugs). Linus has mentioned that Git
    has a particular need for this kind of flag[1]. It also resolves a
    fairly long-standing perceived deficiency in O_NOFOLLOw -- that it only
    blocks the opening of trailing symlinks.
    
    This is part of a refresh of Al's AT_NO_JUMPS patchset[2] (which was a
    variation on David Drysdale's O_BENEATH patchset[3], which in turn was
    based on the Capsicum project[4]).
    
    /* Userspace API. */
    LOOKUP_NO_SYMLINKS will be exposed to userspace through openat2(2).
    
    /* Semantics. */
    Unlike most other LOOKUP flags (most notably LOOKUP_FOLLOW),
    LOOKUP_NO_SYMLINKS applies to all components of the path.
    
    With LOOKUP_NO_SYMLINKS, any symlink path component encountered during
    path resolution will yield -ELOOP. If the trailing component is a
    symlink (and no other components were symlinks), then O_PATH|O_NOFOLLOW
    will not error out and will instead provide a handle to the trailing
    symlink -- without resolving it.
    
    /* Testing. */
    LOOKUP_NO_SYMLINKS is tested as part of the openat2(2) selftests.
    
    [1]: https://lore.kernel.org/lkml/CA+55aFyOKM7DW7+0sdDFKdZFXgptb5r1id9=Wvhd8AgSP7qjwQ@mail.gmail.com/
    [2]: https://lore.kernel.org/lkml/20170429220414.GT29622@ZenIV.linux.org.uk/
    [3]: https://lore.kernel.org/lkml/1415094884-18349-1-git-send-email-drysdale@google.com/
    [4]: https://lore.kernel.org/lkml/1404124096-21445-1-git-send-email-drysdale@google.com/
    
    Cc: Christian Brauner <christian.brauner@ubuntu.com>
    Suggested-by: Al Viro <viro@zeniv.linux.org.uk>
    Suggested-by: Linus Torvalds <torvalds@linux-foundation.org>
    Signed-off-by: Aleksa Sarai <cyphar@cyphar.com>
    Signed-off-by: Al Viro <viro@zeniv.linux.org.uk>

--- include/linux/namei.h
+++ include/linux/namei.h
@@ -46,11 +46,12 @@ enum {LAST_NORM, LAST_ROOT, LAST_DOT, LAST_DOTDOT, LAST_BIND};
 
 #define LOOKUP_JUMPED		0x1000
 #define LOOKUP_ROOT		0x2000
+#define LOOKUP_ROOT_GRABBED	0x0008
 #define LOOKUP_EMPTY		0x4000
 #define LOOKUP_DOWN		0x8000
 
 /* Scoping flags for lookup. */
-#define LOOKUP_NO_SYMLINKS      BIT(24) /* No symlink crossing. */
+#define LOOKUP_NO_SYMLINKS      0x010000 /* No symlink crossing. */
 #define LOOKUP_NO_MAGICLINKS    BIT(25) /* No nd_jump_link() crossing. */
 #define LOOKUP_NO_XDEV          BIT(26) /* No mountpoint crossing. */
 #define LOOKUP_BENEATH          BIT(27) /* No escaping from starting point. */
