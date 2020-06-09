ae30b212df63f4e8e8cdf0f2e1448709757b3f2e
Kees Cook <keescook@chromium.org>
2020-06-09 16:21:38 -0700

    pidfd: Add missing sock updates for pidfd_getfd()
    The sock counting (sock_update_netprioidx() and sock_update_classid())
    was missing from pidfd's implementation of received fd installation. Add
    a call to the new __receive_sock() helper.
    
    Cc: Christian Brauner <christian.brauner@ubuntu.com>
    Cc: Christoph Hellwig <hch@lst.de>
    Cc: Sargun Dhillon <sargun@sargun.me>
    Cc: Jakub Kicinski <kuba@kernel.org>
    Cc: netdev@vger.kernel.org
    Cc: linux-kernel@vger.kernel.org
    Cc: stable@vger.kernel.org
    Fixes: 8649c322f75c ("pid: Implement pidfd_getfd syscall")
    Signed-off-by: Kees Cook <keescook@chromium.org>

--- kernel/pid.c
+++ kernel/pid.c
@@ -42,6 +42,7 @@
 #include <linux/sched/signal.h>
 #include <linux/sched/task.h>
 #include <linux/idr.h>
+#include <net/sock.h>
 
 struct pid init_struct_pid = {
 	.count 		= ATOMIC_INIT(1),
@@ -623,10 +624,12 @@ static int pidfd_getfd(struct pid *pid, int fd)
 	}
 
 	ret = get_unused_fd_flags(O_CLOEXEC);
-	if (ret < 0)
+	if (ret < 0) {
 		fput(file);
-	else
+	} else {
+		__receive_sock(file);
 		fd_install(ret, file);
+	}
 
 	return ret;
 }
